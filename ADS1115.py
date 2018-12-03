9afd6f745c8300e2350bc2d33877733be44a4604
khoirul_muzakka <khoirul.muzakka@gmail.com>
2018-12-03 20:26:31 +0100

    (Hopefully) this is the final version. EVerything has been tested and working perfectly

--- ADS1115.py
+++ ADS1115.py
@@ -55,32 +55,25 @@ def read_config(address = 0x48):
         assert address in address_list
         
         #Receiving configuration data from ADC
-        config = bus.read_word_data (address, pointer_config ) & 0xFFFF  
-        print("Configuration register : {0:016b}".format(config))
+        config = bus.read_word_data (address, pointer_config) & 0xFFFF
+        swapped_config =( (config & 0xFF) << 8) | (config >> 8)
+        print("Configuration register : {0:016b}".format(swapped_config))
         
         # reading channel
-        chnl = (config & 0x7000 ) >> 12
-        for key, value in channel_list.items():
-            if key == chnl :
-                print ("Channel : {0}".format(value))  
+        chnl = channel_list[(swapped_config & 0x7000 ) >> 12]
+        print ("Channel : {0}".format(chnl))  
                 
         #Reading FSR value
-        FSR =  (config & 0x0E00 ) >> 9
-        for key, value in FSR_list.items():
-            if key == FSR:
-                print ("FSR : {} V".format(value))
+        FSR = FSR_list[(swapped_config & 0x0E00 ) >> 9]
+        print ("FSR : {} V".format(FSR))
         
         #Reading operating mode
-        mode =  (config & 0x0100 ) >> 8
-        for key, value in mode_list.items():
-           if key == mode:
-                print ("Operating Mode : {}".format(value))
+        mode = mode_list[(swapped_config & 0x0100 ) >> 8]
+        print ("Operating Mode : {}".format(mode))
                 
         #Reading data rate in SPS
-        D_R =  (config & 0x00E0 ) >> 5
-        for key, value in data_rate_list.items():
-            if key == D_R:
-                print ("Data rate : {} SPS".format(value))
+        D_R = data_rate_list[(swapped_config & 0x00E0 ) >> 5]
+        print ("Data rate : {} SPS".format(D_R))
                 
     except AssertionError:
         print ("Please enter a valid address")
@@ -89,21 +82,25 @@ def read_config(address = 0x48):
                 
     
 def reset (address= 0x48):
-    """ Go back to default setting, can be used to stop ADC from converting in continous mode""" 
+    """ Go back to default setting, can be used to stop ADC from converting in continous mode. The default setting is
+    Channel : AINP = AIN0 and AINN = GND
+    FSR : 4.096 V
+    Operating Mode : Single shot conversion
+    Data rate : 64 SPS""" 
     try :
         assert address in address_list
-        def_config = 0x4583 #channel = 0b100, FSR = 0b010, mode =1, data_rate = 0b100
-        bus.write_word_data(address, pointer_config, def_config )
+        def_config = 0x4363 #channel = 0b100, FSR = 0b001, mode =1, data_rate = 0b011
+        bus.write_i2c_block_data(address, pointer_config, [def_config >> 8, def_config & 0xFF] )
         time.sleep(0.1)
     except AssertionError:
         print ("Please enter a valid address")
-    except IOError :
-        print("Device is not connected")
+    except :
+        print("Error has accoured during writing default configuration register to the ADC")
         
 
 class Ads1115(object) :
-    """Basic functionality of ADS1115"""
-    def __init__(self, address = 0x48, channel = 0b100, FSR =0b001, mode = 1, data_rate = 0b100):
+    """Basic functionalities of ADS1115"""
+    def __init__(self, address = 0x48, channel = 0b100, FSR =0b001, mode = 1, data_rate = 0b011):
         self.address = address
         self.channel = channel
         self.FSR = FSR
@@ -119,70 +116,68 @@ class Ads1115(object) :
           
     def __current_FSR_value(self):
         """Find FSR value given the FSR input""" 
-        for key, value in FSR_list.items():
-            if key == self.FSR :
-                return value
+        return FSR_list[self.FSR]
                  
     def __data_rate_value (self) :
         """find data rate given data rate input """
-        for key, value in data_rate_list.items():
-            if key == self.data_rate:
-                return value 
+        return data_rate_list[self.data_rate]
                
     def __write_config(self):
         """write the configuration register"""
         try :
-            new_config = 0 | (self.channel << 12) | (self.FSR << 9) | (self.mode << 8)|(self.data_rate << 5) | (0 << 4) | (0 << 3) | (0 <<2) |0b11
-            bus.write_word_data(self.address, pointer_config, new_config) 
+            new_config = (1 << 15) | (self.channel << 12) | (self.FSR << 9) | (self.mode << 8)|(self.data_rate << 5) | 0b11
+            bus.write_i2c_block_data(self.address, pointer_config, [new_config >> 8, new_config & 0xFF]) 
             time.sleep(0.1) 
         except IOError :
             print ("Device is not connected")
   
-    def read_raw_data(self):
-        """read raw data output from ads1115 """
+    def raw_data(self):
+        """read raw data output from ads1115. The output is 16bit binary two's complement integers.
+        Note that we write config register first before retrieving raw data. To get the last conversion raw data
+        without first writing config register, use last_raw_data methode"""
         self.__write_config()
         raw_data = bus.read_word_data(self.address, pointer_conversion) & 0xFFFF
         return (raw_data >> 8) | ((raw_data & 0xFF) << 8) #swapping byte order
-
-    def read_raw_data_cont(self):
-        """Read Raw Data in continuous conversion mode, read the raw data without writing config register beforehand"""
-        try :
-            assert self.mode == 0
-            raw_data = bus.read_word_data(self.address, pointer_conversion) & 0xFFFF
-            return (raw_data >> 8) | ((raw_data & 0xFF) << 8)
-        except AssertionError :
-            print ("The device must be in continous conversion mode to run this function")            
-        
-    def just_read(self):
-        """Conversion Result without writing config before reading : useful in continous mode"""      
-        def value ():  #Converting from binary two complement to signed integers"""
-            if self.read_raw_data() & 0x8000 != 0:
-                return self.read_raw_data()- (1 << 16)
-            else :
-                return self.read_raw_data()        
-        return (float(value())/(2**15))*self.__current_FSR_value()
-        
+    
     def read(self) :
-        """ Read the conversion result. The output is in Volts"""                    
+        """ Read the conversion result. The output is in Volts.
+        This command writes config register first then read the conversion. To get the last conversion result without
+        write config register first, use last_conversion methode"""                    
         def value (): #Converting from binary two complement to signed integers
-            if self.read_raw_data() & 0x8000 != 0:
-                return self.read_raw_data()- (1 << 16)
+            if self.raw_data() & 0x8000 != 0:
+                return self.raw_data()- (1 << 16)
             else :
-                return self.read_raw_data()
+                return self.raw_data()
+            
+        return (float(value())/(2**15))*self.__current_FSR_value()
+
+    def last_raw_data(self):
+        """Read raw data from the last conversion without writing config register beforehand. The output is 16bit binary
+        two's complement integers"""
+        raw_data = bus.read_word_data(self.address, pointer_conversion) & 0xFFFF
+        return (raw_data >> 8) | ((raw_data & 0xFF) << 8)
+        
+    def last_read(self):
+        """Read conversion Result from the last cnversion without writing config beforehand. The output is in Volts"""      
+        def value ():  #Converting from binary two complement to signed integers"""
+            if self.last_raw_data() & 0x8000 != 0:
+                return self.last_raw_data()- (1 << 16)
+            else :
+                return self.last_raw_data()    
             
         return (float(value())/(2**15))*self.__current_FSR_value()      
-                   
-    def histogram(self, number_of_samples = 200, bins =50 ):
-        """Draw histogram"""
+                            
+    def histogram_singleshot(self, number_of_samples = 200, bins =50 ):
+        """Draw histogram from a multiple single shot conversions."""
         samples=[]
         for i in range(1,number_of_samples) :
-            sample = self.read_raw_data()
+            sample = self.raw_data()
             samples.append(sample)
             
-        mean_raw_data = sum(samples)/number_of_samples
+        mean_raw_data = np.mean(samples)
         print ("Mean of Raw Data : {0}".format(mean_raw_data))
 
-        RMS = np.sqrt(sum(np.square([float(i) for i in samples]))/number_of_samples)
+        RMS = np.sqrt(np.mean(np.square(samples)))
         print ("RMS : {0}".format(RMS))
         
         std = np.std([float(i) for i in samples])
@@ -193,20 +188,20 @@ class Ads1115(object) :
         plt.xlabel ("Raw Data")
         plt.show()
 
-    def histogram_cont(self, number_of_samples = 200, bins =50 ):
+    def histogram(self, number_of_samples = 200, bins =50 ):
         """Draw histogram in continuous conversion mode"""
         samples=[]
         try :
             assert self.mode == 0
             for i in range(1,number_of_samples) :            
-                sample = self.read_raw_data_cont()
+                sample = self.last_raw_data()
                 time.sleep(0.1)
                 samples.append(sample)
                        
-            mean_raw_data = sum(samples)/number_of_samples
+            mean_raw_data = np.mean(samples)
             print ("Mean of Raw Data : {0}".format(mean_raw_data))
 
-            RMS = np.sqrt(sum(np.square([float(i) for i in samples]))/number_of_samples)
+            RMS = np.sqrt(np.mean(np.square(samples)))
             print ("RMS : {0}".format(RMS))
         
             std = np.std([float(i) for i in samples])
